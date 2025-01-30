d171b2a6c13bb24d7b2c07e14e2687f443535bb5
carlosvfb <cacavfb2107@gmail.com>
2025-01-30 19:52:43 -0300

    ajustando descrição dos pojetos

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,7 @@ import { Layout } from "@/components/layout";
 const projects = [
     {
       title: 'Resgate +',
-      description: 'Resgate+ é um projeto open-source desenvolvido com Next.js e estilizado com Tailwind CSS. Ele foi criado para facilitar a arrecadação de alimentos, roupas e outros itens essenciais para auxiliar pessoas afetadas no Rio Grande do Sul. A plataforma conecta doadores a campanhas e iniciativas de apoio, promovendo solidariedade e engajamento social. 🚀❤️',
+      description: 'Resgate+ é um projeto open-source desenvolvido com Next.js e estilizado com Tailwind CSS. Ele foi criado para facilitar a arrecadação de alimentos, roupas e outros itens essenciais para auxiliar pessoas afetadas no Rio Grande do Sul. A plataforma conecta doadores a campanhas e iniciativas de apoio, promovendo solidariedade e engajamento social.',
       previewImage: '/resgatemais.png',
       previewGif: '/resgatemais.gif',
       codeLink: 'https://github.com/lucaoskaique/resgatemais',
@@ -12,7 +12,7 @@ const projects = [
     },
     {
       title: 'API GitHub',
-      description: 'API GitHub é um projeto feito com HTML, CSS e JavaScript que permite buscar usuários do GitHub por meio da API da plataforma. Ele exibe informações como nome, avatar, bio e repositórios públicos. A interface é responsiva e minimalista, garantindo uma boa experiência. Ideal para explorar perfis rapidamente. 🚀🔥',
+      description: 'API GitHub é um projeto feito com HTML, CSS e JavaScript que permite buscar usuários do GitHub por meio da API da plataforma. Ele exibe informações como nome, avatar, bio e repositórios públicos. A interface é responsiva e minimalista, garantindo uma boa experiência. Ideal para explorar perfis rapidamente.',
       previewImage: '/github.png',
       previewGif: '/github.gif',
       codeLink: 'https://github.com/carlosvfb/projeto-github-api',
@@ -20,7 +20,7 @@ const projects = [
     },
     {
       title: 'Busca das contratações públicas',
-      description: 'Busca das contratações públicas é um site desenvolvido com Next.js e estilizado com Tailwind CSS para buscar contratações públicas. Ele permite filtrar licitações e contratos por uf e a data. A interface é intuitiva e responsiva, garantindo fácil navegação. Os dados são atualizados dinamicamente para transparência. 🚀',
+      description: 'Busca das contratações públicas é um site desenvolvido com Next.js e estilizado com Tailwind CSS para buscar contratações públicas. Ele permite filtrar licitações e contratos por uf e a data. A interface é intuitiva e responsiva, garantindo fácil navegação. Os dados são atualizados dinamicamente para transparência.',
       previewImage: '/pncp.png',
       previewGif: '/pncp.gif',
       codeLink: 'https://github.com/carlosvfb/site-pncp',
@@ -28,7 +28,7 @@ const projects = [
     },
     {
       title: 'Dev Burguer',
-      description: 'Dev Burguer é um cardápio online feito com HTML, CSS e JavaScript para facilitar pedidos. Os clientes podem adicionar itens ao carrinho e enviar o pedido diretamente pelo WhatsApp do estabelecimento. A interface é responsiva e intuitiva, garantindo uma ótima experiência. O sistema simplifica a gestão de pedidos. 🍔🚀',
+      description: 'Dev Burguer é um cardápio online feito com HTML, CSS e JavaScript para facilitar pedidos. Os clientes podem adicionar itens ao carrinho e enviar o pedido diretamente pelo WhatsApp do estabelecimento. A interface é responsiva e intuitiva, garantindo uma ótima experiência. O sistema simplifica a gestão de pedidos.',
       previewImage: '/devburguer.png',
       previewGif: '/devburguer.gif',
       codeLink: 'https://github.com/carlosvfb/projeto-hamburgueria',
